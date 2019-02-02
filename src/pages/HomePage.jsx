2a18080326a57b4b564a2c1a34883d4a97eb066d
Chase Hainsworth <chasehainsworth@gmail.com>
2019-02-01 16:32:16 -0800

    Added extra settings on Homepage Rows.
    Configured for more responsive design & to prevent overlap.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -10,12 +10,12 @@ class HomePage extends Component {
         <h1 className='title'>Safar</h1>
         <h2 style={{ "margin-bottom": 10 }}>Provider Portal</h2>
         <Row type='flex' justify='center' gutter={8}>
-          <Col xs={24} md={2}>
+          <Col xs={24} sm={6} md={4} lg={3} xl={2}>
             <Button type='primary' size='large'>
               Request Access
             </Button>
           </Col>
-          <Col xs={24} md={2}>
+          <Col xs={24} sm={4} md={4} lg={3} xl={4}>
             <Button type='primary' size='large'>
               Login
             </Button>
