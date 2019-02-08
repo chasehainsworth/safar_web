b63a70b07841cb42c8b3af5b35429fe9be08903d
Isaac Taylor <isaactay@usc.edu>
2019-02-08 12:45:33 -0800

    Beginning firebase and routing

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import React, { Component } from "react";
 import "./App.css";
 import "antd/dist/antd.css";
 import Layout from "antd/lib/layout";
+import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-import UpdateAccountPage from "./pages/UpdateAccountPage"
+import AccountPage from "./pages/AccountPage";
+import UpdateAccountPage from "./pages/UpdateAccountPage";
+
 const { Content, Footer } = Layout;
 
 class App extends Component {
@@ -11,8 +14,11 @@ class App extends Component {
     return (
       <Layout className='layout'>
         <Content>
-          {/* <HomePage /> */}
-          <UpdateAccountPage />
+          <Switch>
+            <Route exact path='/' component={HomePage} />
+            <Route path='/UpdateAccount' component={UpdateAccountPage} />
+            <Route path='/Account' component={AccountPage} />
+          </Switch>
         </Content>
         <Footer style={{ textAlign: "right" }}>
           Copyright &copy; 2019 Safar Team
