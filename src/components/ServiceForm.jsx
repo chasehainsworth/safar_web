da6e290ca91c27d37c3878090f435386b021c9f6
Chase Hainsworth <chasehainsworth@gmail.com>
2019-03-01 19:05:33 -0800

    Service loading and modals.
    Services are now loaded into the table. If you click Edit {language}, an
    empty modal will come up and can be closed.

--- src/components/ServiceForm.jsx
+++ src/components/ServiceForm.jsx
@@ -1,44 +1,81 @@
 import React, { Component } from "react";
 import { Table, Upload, Modal, Button } from "antd";
 
-const columns = [{
-    title: 'Language',
-    dataIndex: 'language',
-    key: 'language',
-
-}, {
-    title: 'Name',
-    dataIndex: 'name',
-    key: 'name'
-}, {
-    title: 'Description',
-    dataIndex: 'description',
-    key: 'description'
-}, {
-    title: 'Hours',
-    dataIndex: 'hours',
-    key: 'hours'
-}, {
-    title: 'Action',
-    key: 'action',
-    render: (text, record) => (
-        <a href="#">Edit {record.name}</a>
-    )
-}]
 
 class ServiceForm extends Component {
     constructor(props) {
         super(props);
-        console.log(props.service);
-        // let data = props.service.map(service => {
-        // //    let  
-        // });
+        let dataSource = [];
+        let visible = [];
+        props.service['langs'].forEach( (lang, index) => {
+            dataSource.push({ 
+                key: index, 
+                language: lang.language,
+                name: lang.name,
+                description: lang.description,
+                hours: lang.hours 
+            })
+            visible.push(false);
+        });
+
+        const columns = [{
+            title: 'Language',
+            dataIndex: 'language',
+            key: 'language',
+
+        }, {
+            title: 'Name',
+            dataIndex: 'name',
+            key: 'name'
+        }, {
+            title: 'Description',
+            dataIndex: 'description',
+            key: 'description'
+        }, {
+            title: 'Hours',
+            dataIndex: 'hours',
+            key: 'hours'
+        }, {
+            title: 'Action',
+            key: 'action',
+            render: (text, record) => (
+                <div>
+                    <a onClick={() => this.setVisible(record.key, true) }>Edit {record.language}</a>
+                </div>
+            )
+        }]
+
+        this.state = {
+            columns,
+            dataSource,
+            visible
+        }
     }
 
+    setVisible = (key, value) => {
+        let visible = [...this.state.visible];
+        visible[key] = value;
+        this.setState({ visible });
+    }
+    
     render() {
         return (
             <div>
-                <Table columns={columns} /> 
+                <Table columns={this.state.columns} dataSource={this.state.dataSource} /> 
+                {
+                    this.state.visible.map( (visible, index) => {
+                        return (
+                        <Modal
+                            key={index}
+                            title={`Edit ${this.state.dataSource[index].language}`}
+                            visible={visible}
+                            onCancel={() => this.setVisible(index, false)}
+                        >
+                        Content!
+                        </Modal>
+                        )
+                    })
+                }  
             </div>
         );
     }
