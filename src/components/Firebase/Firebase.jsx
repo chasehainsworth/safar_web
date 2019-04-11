6b25a8511651993be2395c2ed0c5570afe5f7673
Chase Hainsworth <chasehainsworth@gmail.com>
2019-04-10 21:17:03 -0700

    Another missed line

--- src/components/Firebase/Firebase.jsx
+++ src/components/Firebase/Firebase.jsx
@@ -2,6 +2,7 @@ import app from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 import "firebase/storage";
+import "firebase/functions";
 
 const config = {
   apiKey: process.env.REACT_APP_API_KEY,
