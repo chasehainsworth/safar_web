57d94ed110a8976132216e6107ab13cbf76f68e1
Chase Hainsworth <chasehainsworth@gmail.com>
2019-04-10 21:08:28 -0700

    This file was missed in the previous commit

--- src/components/Firebase/Firebase.jsx
+++ src/components/Firebase/Firebase.jsx
@@ -26,6 +26,7 @@ class Firebase {
     this.auth = app.auth();
     this.db = app.firestore();
     this.storage = app.storage();
+    this.functions = app.functions();
     //this.db.settings({ timestampsInSnapshots: true });
   }
 
