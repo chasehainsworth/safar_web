8534402b9831298b11c971f65a81a48d84e9d55d
Isaac Taylor <isaactay@usc.edu>
2019-04-15 10:51:08 -0700

    Add precedence for hierarchy in doc.

--- src/components/CategoriesForm.jsx
+++ src/components/CategoriesForm.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 import StepFormComponent from "./StepFormComponent";
 import { Form, Select, Row, Col, Checkbox, Divider } from "antd";
 import CheckboxGroup from "antd/lib/checkbox/Group";
@@ -39,7 +39,9 @@ const initial = null;
 /**
  * Form with a multi-select categories dropdown, with at least one required category. For each selected category, a row
  * will appear with checkboxes for each subcategory.
- * 
+ *
+ * Parent: [StepFormComponent](#stepformcomponent)
+ *
  * The hardcoded categories and subcategories are:
  * * Health: Pregnancy, Dental, Emergency, Shots/Vaccines, Hospitals/Clinics, General, Specialized, Hygiene
  * * Education: Computer, Language, Child Education, Adult Education, Writing, Health
@@ -47,7 +49,6 @@ const initial = null;
  * * Entertainment: Children's Rights, Domestic Violence, Family, Single Males, Protection, Asylum
  */
 class CategoriesForm extends StepFormComponent {
-
   constructor(props) {
     super(props);
 
@@ -60,11 +61,11 @@ class CategoriesForm extends StepFormComponent {
   }
 
   /**
- * Sets categories in state to update rows displayed.
- *
- * @param {array} values
- * @public
- */
+   * Sets categories in state to update rows displayed.
+   *
+   * @param {array} values
+   * @public
+   */
   handleCatChange(values) {
     this.setState({ categories: [...values] });
   }
@@ -83,9 +84,9 @@ class CategoriesForm extends StepFormComponent {
         <Form.Item
           validateStatus={noCatError ? "error" : ""}
           help={noCatError || ""}
-          labelCol={{span: 11, textAlign: "right"}}
-          wrapperCol={{span: 5, offset: 1}}
-          label="Choose Categories"
+          labelCol={{ span: 11, textAlign: "right" }}
+          wrapperCol={{ span: 5, offset: 1 }}
+          label='Choose Categories'
         >
           {getFieldDecorator("categories", {
             rules: [{ required: true, message: "Select 1 or more categories" }]
@@ -102,37 +103,39 @@ class CategoriesForm extends StepFormComponent {
             </Select>
           )}
         </Form.Item>
-        {this.state.categories
-          .map((cat, index) => {
-            return (
-              <Row>
-                <Col span={6} style={{fontWeight: "bold"}}>
-                  {cat}
-                </Col>
-                <Col span={18} style={{textAlign: "left"}}>
-                  <Form.Item>
-                    {getFieldDecorator(cat + "Tags", { initialValue: initial })(
-                      <CheckboxGroup style={{width: "100%"}}>
-                        {tags[cat].map(tag => <Col span={8}><Checkbox value={tag}>{tag}</Checkbox></Col>)}
-                      </CheckboxGroup>
-                    )}
-                  </Form.Item>
-                </Col>
-                {index !== this.state.categories.length - 1 && (<Divider />) }
-              </Row>
-
-            );
-          })}
+        {this.state.categories.map((cat, index) => {
+          return (
+            <Row>
+              <Col span={6} style={{ fontWeight: "bold" }}>
+                {cat}
+              </Col>
+              <Col span={18} style={{ textAlign: "left" }}>
+                <Form.Item>
+                  {getFieldDecorator(cat + "Tags", { initialValue: initial })(
+                    <CheckboxGroup style={{ width: "100%" }}>
+                      {tags[cat].map(tag => (
+                        <Col span={8}>
+                          <Checkbox value={tag}>{tag}</Checkbox>
+                        </Col>
+                      ))}
+                    </CheckboxGroup>
+                  )}
+                </Form.Item>
+              </Col>
+              {index !== this.state.categories.length - 1 && <Divider />}
+            </Row>
+          );
+        })}
       </div>
     );
   }
 }
 
 CategoriesForm.propTypes = {
-  /** Form data retrieved from Firebase or entered by user */
+  /** *Inherited:* Form data retrieved from Firebase or entered by user */
   formData: PropTypes.object,
-  /** Antd form object */
+  /** *Inherited:* Antd form object */
   formObject: PropTypes.object
-}
+};
 
 export default CategoriesForm;
